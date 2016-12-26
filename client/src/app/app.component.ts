d6cca421b9a959dd492d6b647fab9fa574510fe3
Jedediah Smith <jed@obdstudios.com>
2016-12-26 15:10:49 -0700

    Added the angular-cli build to the client folder.

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+
+
+
+  // Test Accordion
+  public items:string[] = ['Item 1', 'Item 2', 'Item 3'];
+ 
+}
