171902e7e8750b77d3bb55c8cb9458013d9e6437
James Jensen <james@obdstudios.com>
2018-01-23 20:16:39 -0700

    example email reset password

--- common/models/users.js
+++ common/models/users.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Users) {
 
+  User.on('resetPasswordRequest', function(info) {
+    var url = 'http://' + config.host + ':' + config.port + '/reset-password';
+    var html = 'Click <a href="' + url + '?access_token=' +
+      info.accessToken.id + '">here</a> to reset your password';
+
+    User.app.models.Email.send({
+      to: info.email,
+      from: senderAddress,
+      subject: 'Password reset',
+      html: html
+    }, function(err) {
+      if (err) return console.log('> error sending password reset email');
+      console.log('> sending password reset email to:', info.email);
+    });
+  });
+
 };
